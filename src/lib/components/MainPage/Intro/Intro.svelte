5f31a15eb41eb73e3ba3ac2f11f8b54b69881903
Enochen <sirenochen@gmail.com>
2023-08-25 17:43:33 -0400

    use svelte store and split up components

---
+++ src/lib/components/MainPage/Intro/Intro.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import IntroDesc from './IntroDesc.svelte';
+	import IntroTitle from './IntroTitle.svelte';
+</script>
+
+<div class="my-auto flex flex-col items-center justify-center gap-12 transition-all">
+	<IntroTitle />
+	<IntroDesc />
+</div>
