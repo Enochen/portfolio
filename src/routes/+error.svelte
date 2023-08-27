c4eb7543f144bed740c59f0667a6c6e081926b11
Enochen <sirenochen@gmail.com>
2023-08-27 01:32:51 -0400

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+
+<div class="container mx-auto flex h-full flex-col justify-center items-center gap-4">
+  <h1 class="h1 text-5xl">{$page.status}: {$page.error?.message}</h1>
+  <h2 class="h2 text-3xl">oh no 😱</h2>
+</div>
